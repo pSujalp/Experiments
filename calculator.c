ce54044e3539de75a0e9d3b7e63b4f54a8cab4c4
pSujalp <108971732+pSujalp@users.noreply.github.com>
2022-12-08 18:24:53 +0530

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    float a,b,c,d;
+    float a,b;
+    int c,d;
     scanf("%f %f",&a,&b);
-    scanf("%f %f",&c,&d);
+    c=a;
+    d=b;
     printf("The sum is %f \n",a+b);
     printf("The diff is %f \n",a-b);
-    printf("The division is %lf \n",c/d);
+    printf("The division is %lf \n",a/b);
+    printf("The modulus is %d",c%d);
     printf("The multiplication  is %f \n",a*b);
     return 0;
 
