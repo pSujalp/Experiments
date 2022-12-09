c5ccf85f934dad87ef1efd12a3354585f080e076
pSujalp <108971732+pSujalp@users.noreply.github.com>
2022-12-09 11:45:32 +0530

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -3,9 +3,11 @@ int main()
 {
     float a,b;
     int c,d;
+        printf("Enter two number's\n");
     scanf("%f %f",&a,&b);
     c=a;
     d=b;
+
     printf("The sum is %f \n",a+b);
     printf("The diff is %f \n",a-b);
     printf("The division is %lf \n",a/b);
